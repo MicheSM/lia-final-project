cbca6d601a44de43f0d254f58d2697e1ee69340a
Mykhaylo Semenyshen <m.semenyshen@studenti.unipi.it>
2025-08-24 16:08:57 +0200

    add representative images of dataset and script to visualize data distribution with bar plot
    Add EDA.ipynb with bar plot of every Plant-Disease on x and Count on Y
    Add images folder for every Plant___Disease with the first 3 photos of the dataset
    Modfy data_analysis.py: commented out scripts that are no longer useful like folder creation, file copy, file ':Zone.identifier' deletion

--- app/data_analysis.py
+++ app/data_analysis.py
@@ -2,14 +2,24 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from pathlib import Path
 import json
+import shutil
+import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-IMAGE_PATH = BASE_DIR / 'dataset' / 'color'
+IMAGE_SRC_PATH = BASE_DIR / 'dataset' / 'color'
 DATA_PATH = BASE_DIR / 'app' / 'data' / 'classes.json'
+IMAGE_DEST_PATH = BASE_DIR / 'app' / 'data' / 'images'
 
+# remove all :Zone.Identifier files
 #
-# loop through dataset and write json to a file with this structure
-#
+#for item in IMAGE_SRC_PATH.iterdir():
+#    for file in item.iterdir():
+#        if file.name.__contains__(":Zone.Identifier"):
+#            os.remove(file)
+
+
+# loop through dataset and write json to a file with this structure 
+# 
 # plant type 1 :
 #   health condition 1 : number of pictures
 #   health condition 2 : ...
@@ -17,11 +27,34 @@ DATA_PATH = BASE_DIR / 'app' / 'data' / 'classes.json'
 # plant type 2 :
 #   ...
 #
-data = {}
-for item in IMAGE_PATH.iterdir():
-    name, status  = item.name.split('___')
-    if not data.get(name):
-        data[name] = {}
-    data[name][status] = len(list(item.iterdir()))
+#data = {}
+#for item in IMAGE_SRC_PATH.iterdir():
+#
+#    name, status  = item.name.split('___')          # get name and status from folder name
+#    if not data.get(name):
+#        data[name] = {}                             # create dict for plant type if not exists 
+#    data[name][status] = len(list(item.iterdir()))  # count number of images in folder and save it
+#
+#DATA_PATH.write_text(json.dumps(data, indent=2))
 
-DATA_PATH.write_text(json.dumps(data, indent=2))
+
+# for every case save 3 images in folders created in IMAGE_DEST_PATH / correct folder
+#
+#for item in IMAGE_SRC_PATH.iterdir():
+#    dest_dir = IMAGE_DEST_PATH / item.name
+#    dest_dir.mkdir(parents=True, exist_ok=True)
+#    copied = 0
+#    for file in item.iterdir():
+#        if copied >= 3:
+#           break
+#        if not file.is_file():
+#            continue
+#        if ":Zone.Identifier" in file.name:
+#            continue
+#        try:
+#            shutil.copy(file, dest_dir / file.name)
+#            copied += 1
+#        except Exception:
+#            continue
+
+
